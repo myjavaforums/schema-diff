463453b22ac7cf611011383e75a8166be5e09f70
fuller.sethm <fuller.sethm@e7384a63-1753-0410-8deb-41c56c76d84b>
2008-07-29 15:04:17 +0000

    Initial checkin of application starter

---
+++ org/schema_diff/database/DatabaseConnection.java
@@ -0,0 +1,201 @@
+package org.schema_diff.database;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+
+import java.util.Properties;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.HashMap;
+
+
+import org.schema_diff.AppSupport;
+
+public class DatabaseConnection {
+
+    private HashMap connectionMap = new HashMap();
+
+    public static String propFile = "config/schema_diff.properties";
+
+    private static DatabaseConnection _dbConn = null;
+    private static Properties _config = null;
+
+
+    public static DatabaseConnection getInstance() {
+        if ( _dbConn == null ) {
+	
+
+	    _config = AppSupport.getProperties();
+	    if ( _config == null ) {
+		return null;
+	    }
+
+	    _dbConn = new DatabaseConnection(_config);
+	}
+        return _dbConn;
+    }
+    
+    private DatabaseConnection(Properties config)
+    {
+        _config = config;
+    }
+    
+
+    public Connection getConnection(String connName) {
+	Connection _connection = (Connection) connectionMap.get(connName);
+        if ( _connection != null ) {
+	    return _connection;
+	}
+        
+        try {
+	    Properties connProps = new Properties();
+	    String connDriverName = connName + ".driver";
+	    String connDriverClass = _config.getProperty(connDriverName);
+	    System.err.println(connDriverName + " = " + connDriverClass);
+
+	    Driver driver
+		= (Driver)Class.forName(connDriverClass).newInstance();
+	    String connUser = connName + ".user";
+	    String connUserName = _config.getProperty(connUser);
+	    System.err.println(connUser + " = " + connUserName);
+	    connProps.setProperty("user", connUserName);
+
+	    String connPass = connName + ".password";
+	    String connPassVal = _config.getProperty(connPass);
+	    System.err.println(connPass + " = " + connPassVal);
+	    connProps.setProperty("password", connPassVal);
+
+	    String connUrl = connName + ".url";
+	    String connUrlVal = _config.getProperty(connUrl);
+	    System.err.println(connUrl + " = " + connUrlVal);
+	    _connection = driver.connect(connUrlVal, connProps);
+	    _connection.setAutoCommit(true);
+            
+
+	    connectionMap.put(connName, _connection);
+	}
+        catch ( Exception ex ) {
+	    _connection = null;
+	    ex.printStackTrace();
+	}
+        return _connection;
+    }
+    
+    public void exec(String sql)
+	throws SQLException
+    {
+        PreparedStatement ps = getConnection().prepareStatement(sql);
+        ps.execute();
+        close(ps,null);
+    }
+    
+    public CallableStatement prepareCall(String callStatement)
+	throws SQLException {
+	CallableStatement callStmt
+	    = getConnection().prepareCall(callStatement);
+
+	return callStmt;
+
+    }
+
+    public PreparedStatement prepareStatement(String schemaName, 
+					      String prepStatement)
+	throws SQLException {
+	PreparedStatement prepStmt
+	    = getConnection(schemaName).prepareStatement(prepStatement);
+
+	return prepStmt;
+
+    }
+
+    public long getId(String sql)
+	throws SQLException
+    {
+        long id = 0;
+        PreparedStatement ps = getConnection().prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        if ( rs.next() )
+	    {
+		id = rs.getLong(1);
+	    }
+        close(ps,rs);
+        return id;
+    }
+    
+    public ResultSet getResultSet(String schemaName, String sql)
+	throws SQLException {
+        long id = 0;
+        PreparedStatement ps
+	    = getConnection(schemaName).prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+	//	close(ps, null);
+        return rs;
+    }
+    
+    public int executeUpdate(String sql) 
+	throws SQLException {
+        long id = 0;
+	int rc = 0;
+	ResultSet rs = null;
+        Statement stmt = getConnection().createStatement();
+        rc = stmt.executeUpdate(sql);
+
+        return rc;
+    }
+    
+    public void close(
+		      PreparedStatement ps,
+		      ResultSet rs) {
+        if(rs!=null)
+	    {
+		try
+		    {
+			rs.close();
+		    }
+		catch(SQLException ex)
+		    {
+		    }
+	    }
+        if(ps!=null)
+	    {
+		try
+		    {
+			ps.close();
+		    }
+		catch(SQLException ex1)
+		    {
+		    }
+	    }
+    }
+    
+    public void closeAllConnections()
+	throws SQLException
+    {
+	Set connKeys = connectionMap.keySet();
+	Iterator connIter = connKeys.iterator();
+	while (connIter.hasNext()) {
+	    String connName = (String) connIter.next();
+	    Connection _connection = (Connection) connectionMap.get(connName);
+	    if ( _connection != null ) {
+		_connection.close();
+		_connection = null;
+	    }
+	}
+    }
+    
+
+}
+
+
